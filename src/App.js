74639c9409d8299cbb89f4a075ea7ba5bde1ec73
senaru <s3696653@student.rmit.edu.au>
2020-07-08 23:55:18 +1000

    Creating App Component and Creating Pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
+
+import HomePage from './pages/HomePage'
+import AboutPage from './pages/AboutPage'
+import ArticlesList from './pages/ArticlesList'
+import ArticlePage from './pages/ArticlePage'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Route path="/" component={HomePage} exact />
+          <Route path="/about" component={AboutPage} />
+          <Route path="/articles" component={ArticlesList} />
+          <Route path="/article" component={ArticlePage} />
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
