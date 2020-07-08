faf52790bb59c4cbf866c24f4feb5fa652d50bf0
senaru <s3696653@student.rmit.edu.au>
2020-07-09 00:22:17 +1000

    URL Params with react-router
    Change CSS

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import {
   Route
 } from 'react-router-dom';
 
-import HomePage from './pages/HomePage'
-import AboutPage from './pages/AboutPage'
-import ArticlesList from './pages/ArticlesList'
-import ArticlePage from './pages/ArticlePage'
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ArticlesList from './pages/ArticlesList';
+import ArticlePage from './pages/ArticlePage';
+import Nav from './Nav';
+
 import './App.css';
 
 class App extends Component {
@@ -15,10 +17,13 @@ class App extends Component {
     return (
       <Router>
         <div className="App">
-          <Route path="/" component={HomePage} exact />
-          <Route path="/about" component={AboutPage} />
-          <Route path="/articles" component={ArticlesList} />
-          <Route path="/article" component={ArticlePage} />
+          <Nav />
+          <div id="page-body">
+            <Route path="/" component={HomePage} exact />
+            <Route path="/about" component={AboutPage} />
+            <Route path="/articles" component={ArticlesList} />
+            <Route path="/article/:name" component={ArticlePage} />
+          </div>
         </div>
       </Router>
     );
--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,16 +1,24 @@
 import React from 'react';
+import articleContent from './article-content';
 
-const ArticlePage = () => (
-    <>
-        <h1>Test Article</h1>
-        <p>
-            "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
-    </p>
+const ArticlePage = ({ match }) => {
+    const name = match.params.name;
+    const article = articleContent.find(article => article.name == name)
 
-        <p>
-            "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
-    </p>
-    </>
-);
+    if (!article) {
+        return (
+            <h1>
+                Article Does not exist!
+            </h1>
+        )
+    }
+
+    return (
+        <>
+            <h1>{article.title}</h1>
+            {article.content.map((p, k) => (<p key={k}>{p}</p>))}
+        </>
+    );
+}
 
 export default ArticlePage;
