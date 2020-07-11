c85a41010bcdd1ef8cf5440349938191f61dd267
senaru <s3696653@student.rmit.edu.au>
2020-07-11 21:19:05 +1000

    Adding Hooks

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import articleContent from './article-content';
 import NotFoundPage from './NotFoundPage';
 import ArticlesList from '../components/ArticlesList';
 
 const ArticlePage = ({ match }) => {
+
     const name = match.params.name;
     const article = articleContent.find(article => article.name == name)
     const recommendations = articleContent.filter(article => article.name != name)
+
+    const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] })
+
+    useEffect(() => setArticleInfo({ upvotes: 3 }))
 
     if (!article) {
         return (
@@ -18,6 +23,7 @@ const ArticlePage = ({ match }) => {
         <>
             <h1>{article.title}</h1>
             {article.content.map((p, k) => (<p key={k}>{p}</p>))}
+            <em>Upvotes: {articleInfo.upvotes}</em>
             <h2>Recommendations:</h2>
             <ArticlesList articles={recommendations} />
         </>
